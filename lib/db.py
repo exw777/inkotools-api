549d25e19598471e5794891c45c66e4b7d4fcc3d
Kirill Troitskiy <kirill@troitskiy.me>
2022-01-10 21:11:00 +0300

    Refactor db module

--- lib/db.py
+++ lib/db.py
@@ -2,31 +2,38 @@
 # lib/db.py
 
 # internal imports
-import asyncio
-from contextlib import contextmanager
 import logging
 import pathlib
 import sqlite3
-from time import time
+from collections import namedtuple
 
 # external imports
 import netaddr
 
 # local imports
-from .config import ROOT_DIR, NETS
-from .sw import Switch, batch_async
+from .config import ROOT_DIR
 
 # module logger
 log = logging.getLogger(__name__)
 
-# debug
-log.setLevel(10)
-
-DATA_PATH = ROOT_DIR/'data'
-DATABASE = DATA_PATH/'switches2.db'
-
 
 class DB:
+    """Local switches sqlite database
+
+    Stored in `data` directory. Filename is defined in init.
+
+    Inside database:
+    ip stored in integer format (e.g. 2130706433)
+    mac stored in plain text (for fuzzy search)
+    as bytes without any colons `:` or hyphens `-` (e.g. 0123456789AB)
+
+    In output functions:
+    ip displayed as integer octets separated by dots (e.g. 127.0.0.1),
+    mac displayed as bytes separated by hyphens (e.g. 01-23-45-67-89-AB)
+
+    """
+
+    # some multiline sql queries used in class
 
     SCHEMA = '''CREATE TABLE IF NOT EXISTS switches (
             ip integer PRIMARY KEY,
@@ -53,29 +60,24 @@ class DB:
                 '''
 
     def __init__(self, db_name):
-
-        # Check datadir exists
+        # create datadir if not exists
         db_path = ROOT_DIR/'data'
         pathlib.Path(db_path).mkdir(parents=True, exist_ok=True)
-
+        # declare database file to open
         self.db_file = db_path.joinpath(db_name).with_suffix('.db')
-
         # declare connection status flag
         self.is_connected = False
-
-        # check database exists
+        # exec initial sql if database file not exists
         if not self.db_file.exists():
             log.warning('No db file found, creating new...')
-            if self._exec(self.SCHEMA):
+            if self._exec(self.SCHEMA) is not None:
                 self._connection.commit()
                 log.info('New database created')
 
-        log.debug('DB class created')
-
     def __del__(self):
+        # close database on exit
         if self.is_connected:
             self._close()
-        log.debug('DB class deleted')
 
     def _open(self):
         try:
@@ -120,72 +122,91 @@ class DB:
 
     def add(self, sw):
         """Add new switch or update changes
+
+        sw: can be either Switch class object,
+            either dict of strings (ip, mac, model, location)
+
         Returns:
-        1 - Added or updated
-        0 - Skipped (no changes)
+            1 - Added or updated
+            0 - Skipped (no changes)
         """
 
+        # check sw type, convert dict to named tuple
+        if isinstance(sw, dict):
+            sw = namedtuple('Switch', sw)(**sw)
+
         query = self.UPSERT.format(
-            ip=int(sw.ip), mac=sw.mac.format(netaddr.mac_bare),
-            model=sw.model, location=sw.location)
+            ip=int(netaddr.IPAddress(sw.ip)),
+            mac=netaddr.EUI(sw.mac).format(netaddr.mac_bare),
+            model=sw.model,
+            location=sw.location
+        )
         result = self._exec(query)
-        if result:
+        if result is not None:
             if result.rowcount == 0:
-                log.debug(f'{sw.ip} already exists, skipping')
+                log.debug(f'{sw.ip} already exists')
             else:
                 self._connection.commit()
                 log.info(f'{sw.ip} added to database')
             return result.rowcount
         else:
-            log.error(f'adding {sw.ip} failed')
+            log.error(f'{sw.ip} failed')
+            return None
+
+    def add_many(self, sw_list):
+        """Same as add, but for list of switches
+
+        Returns: count of added/updated items
+        """
+        cnt = 0
+        for sw in sw_list:
+            cnt += self.add(sw)
+        log.info(f'Added {cnt} items')
+        return(cnt)
+
+    def get(self, sw_ip):
+        """Get switch entry
+
+        Returns: dict of strings (ip, mac, model, location)
+        """
+        query = "SELECT * from switches WHERE ip = '{ip}';".format(
+            ip=int(netaddr.IPAddress(sw_ip)))
+        result = self._exec(query).fetchone()
+        if result is not None:
+            return self._row_format(result)
+        else:
+            log.error(f'{sw_ip} failed')
             return None
 
     def delete(self, sw_ip):
         """Delete switch by ip
+
         Returns:
-        1 - Deleted
-        0 - Skipped (no changes)
+            1 - Deleted
+            0 - Skipped (no changes)
         """
         query = "DELETE from switches WHERE ip = '{ip}';".format(
             ip=int(netaddr.IPAddress(sw_ip)))
         result = self._exec(query)
-        if result:
+        if result is not None:
             if result.rowcount == 0:
-                log.debug(f'{sw_ip} already deleted, skipping')
+                log.debug(f'{sw_ip} already deleted')
             else:
                 self._connection.commit()
                 log.info(f'{sw_ip} deleted from database')
             return result.rowcount
         else:
-            log.error(f'deleting {sw_ip} failed')
+            log.error(f'{sw_ip} failed')
             return None
 
-    def update(self):
-        """Add all available switches to database"""
-        # TODO: check tainted swithces
-        log.info('Database generation started')
-        cnt = 0
-        start = time()
-        # asyncio.run(batch_async(NETS, self.add, external=True))
-        # TODO: async run, generate sql query and run it once
-        for ip in NETS:
-            try:
-                sw = Switch(ip)
-            except Switch.UnavailableError:
-                log.debug(f'Skipping {ip}')
-            else:
-                log.debug(f'Adding {ip}')
-                if self.add(sw):
-                    cnt += 1
-        end = time() - start
-        log.info(
-            f'Database generation finished in {end:.2f}s (added {cnt} items)')
-        return cnt
-
     def clear(self):
+        """Flush all entries
+
+        Returns: count of deleted items
+        """
         query = "DELETE from switches;"
         result = self._exec(query)
-        if result:
+        if result is not None:
             if result.rowcount == 0:
                 log.debug('No changes')
             else:
@@ -197,7 +218,10 @@ class DB:
             return None
 
     def search(self, word):
-        """Search by mac, model or location"""
+        """Search by mac, model or location
+
+        Returns: list of dicts of strings (ip, mac, model, location)
+        """
         result = []
         # for mac search without '-' and ':'
         query = self.SEARCH.format(
@@ -207,6 +231,7 @@ class DB:
         return result
 
     def ip_list(self):
+        """Get list of all ip addresses"""
         result = []
         query = 'SELECT ip from switches ORDER BY ip;'
         for row in self._exec(query):
